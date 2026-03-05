3421ccfdeca49ef43f69d7d86ede41e0057e7404
patoliyarp <patoliya.rp@gmail.com>
2026-03-05 11:45:15 +0530

    refactor blog schema

--- src/schema/BlogSchema.js
+++ src/schema/BlogSchema.js
@@ -2,11 +2,11 @@ import * as Yup from 'yup';
 
 export const blogSchema = Yup.object().shape({
   title: Yup.string()
-    .min(10, 'Title must be 10 or more character')
+    .min(10, 'title must grater than 10 character')
     .max(150, 'Title must be 150 or less character')
     .required('Title is required'),
   content: Yup.string()
-    .min(500, 'Blog mus be 500 or more character')
+    .min(250, '250 character must be required')
     .required('Blog is required'),
   category: Yup.string()
     .oneOf(['Marketing', 'Sales', 'Business', 'Technology'])
